22d8a6da28138cc9024a3567257d7f0f946ed3ed
@christinaszele <43682308+cszele@users.noreply.github.com>
2019-07-03 15:02:56 -0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,14 +1,26 @@
 /*
  * Create a list that holds all of your cards
  */
+ // array of cards
+ let cards = ['fa-diamond', 'fa-diamond',
+              'fa-paper-plane-o', 'fa-paper-plane-o',
+              'fa-anchor', 'fa-anchor',
+                'fa-bomb', 'fa-bomb',
+               'fa-leaf', 'fa-leaf',
+              'fa-bolt', 'fa-bolt',
+              'fa-cube', 'fa-cube',
+              'fa-bicycle', 'fa-bicycle'];
 
-
+var modal = document.getElementById("myModal");
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+function generateCard(card) {
+    return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
+  }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -24,27 +36,51 @@ function shuffle(array) {
 
     return array;
 }
-createDeck()
-function createDeck() {
-	var cardList = document.querySelectorAll('.card');
-	const cardArray = Array.from(cardList);
-	const randArray = shuffle(cardArray);
-	for (i=0; i < cardList.length; i++) {  
-		const oldCard = cardList[i];
-		oldCard.remove(); 
-	}
-	const deckFragment = document.createDocumentFragment();
-	for (i=0; i < randArray.length; i++) {
-		const newCard = document.createElement('li');
-		newCard.innerHTML = randArray[i].innerHTML; 
-		newCard.className = randArray[i].className;
-		deckFragment.appendChild(newCard);
-	}
-	const newDeckList = document.querySelector('.deck');
-	newDeckList.appendChild(deckFragment);
+
+// Stars
+function showStars(moveCounter) {
+    let stars = document.querySelector('.stars');
+    if (moveCounter == 4) {
+      stars.removeChild(stars.lastElementChild);
+    } else if (moveCounter == 8) {
+      stars.removeChild(stars.lastElementChild);
+    }
+  };
+
+  function pad(val) {
+    var valString = val + "";
+    if (valString.length < 2) {
+      return "0" + valString;
+    } else {
+      return valString;
+    }
+  }
+
+//generates the cards to the game
+function gameOn() {
+  let deck = document.querySelector('.deck');
+  let cardHTML = shuffle(cards).map(function(card) {
+    return generateCard(card);
+  });
+//place the cards to the html
+  deck.innerHTML = cardHTML.join('');
 }
 
+// timer
+var minutesLabel = document.getElementById("minutes");
+var secondsLabel = document.getElementById("seconds");
+var totalSeconds = 0;
 
+function setTime() {
+  ++totalSeconds;
+  secondsLabel.innerHTML = pad(totalSeconds % 60);
+  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+// Play the game
+gameOn();
+// Checking if the flipped cards match
+checkingCards();
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -56,3 +92,90 @@ function createDeck() {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+function checkingCards() {
+  let allCards = document.querySelectorAll('.card');
+  let openCards = [];
+  let matchedCards = 0;
+  let moves = document.querySelector('.moves');
+  let moveCounter = 0;
+  let cardsMatched = false;
+
+  moves.innerHTML = moveCounter;
+  let timer = setInterval(setTime, 1000);
+
+  allCards.forEach(function(card) {
+   card.addEventListener('click', function(e) {
+     if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
+       openCards.push(card);
+       card.classList.add('open', 'show');
+      // if cards match leave them open
+           if (openCards.length == 2) {
+               if (openCards[0].dataset.card == openCards[1].dataset.card) {
+                  openCards[0].classList.add('match');
+                  openCards[0].classList.add('open');
+                  openCards[0].classList.add('show');
+
+                  openCards[1].classList.add('match');
+                  openCards[1].classList.add('open');
+                  openCards[1].classList.add('show');
+
+                  openCards = [];
+                  matchedCards ++;
+                  if (matchedCards == 8) {
+                    cardsMatched = true;
+                    clearInterval(timer);
+                    allCardsMatch();
+                  }
+                }
+                else  {
+                //if cards don't match Hide
+                    setTimeout(function() {
+                      openCards.forEach(function(card) {
+                        card.classList.remove('open', 'show');
+                      });
+                openCards = [];
+
+              }, 1000);
+            }
+            // if all cards are not matched yet
+            if (cardsMatched == false) {
+              moveCounter++;
+              moves.innerHTML = moveCounter;
+              showStars(moveCounter);
+            }
+          }
+        }
+    });
+  });
+};
+
+  //modal
+  function allCardsMatch () {
+    gameOver();
+    modal.style.display = "block";
+  }
+
+ function gameOver() {
+   let time_results = document.querySelector('.time_results');
+   let move_results = document.querySelector('.moves_results');
+   let stars_results = document.querySelector('.stars_results');
+
+   time_results.innerHTML = totalSeconds;
+   move_results.innerHTML = document.querySelector('.moves').innerHTML;
+   stars_results.innerHTML = document.querySelector('.stars').innerHTML;
+ }
+
+//replay
+  let replay = document.querySelector('.fa-repeat');
+  replay.addEventListener('click', function(e) {
+    gameOn();
+    checkingCards();
+  });
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
